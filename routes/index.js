0405b8e5464588a2ec03aecc8206dbcd6ca8fda7
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-09-25 14:35:38 -0500

    Serve profile pic from data.json

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const data = require("../data.json");
+const profilePic = data.profilePic;
 const projects = data.projects.reverse();
 
 // Home
 router.get("/", (req, res, next) => {
-  res.render("index", { projects });
+  res.render("index", { projects, profilePic });
 });
 
 // About
 router.get("/about", (req, res, next) => {
-  res.render("about");
+  res.render("about", { profilePic });
 });
 
 // Projects
@@ -18,7 +19,7 @@ router.get("/projects/:id", (req, res, next) => {
   const id = req.params.id;
   const project = projects.find((project) => project.id === id);
   if (project) {
-    res.render("project", { project });
+    res.render("project", { project, profilePic });
   } else {
     const err = new Error("The project you requested does not exist");
     err.status = 404;
